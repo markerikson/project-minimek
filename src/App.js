8ef98cb2ef0dafcb4b6229a8bf79d89d93ef95c5
Mark Erikson <mark@isquaredsoftware.com>
2016-11-14 22:57:02 -0500

    Add initial dummy tab panel components

--- src/App.js
+++ src/App.js
@@ -6,16 +6,24 @@ import {
 
 import TabBar from "./components/TabBar";
 
-
 import './App.css';
+
+
+const UnitInfo = () => <div>Unit Info content</div>;
+
+const Pilots = () => <div>Pilots content</div>;
+
+const Mechs = () => <div>Mechs content</div>;
+
+const UnitOrganization = () => <div>Unit Organization content</div>;
 
 class App extends Component {
     render() {
         const tabs = [
-            {name : "unitInfo", label : "Unit Info (test)"},
-            {name : "pilots", label : "Pilots"},
-            {name : "mechs", label : "Mechs"},
-            {name : "unitOrganization", label : "Unit Organization"}
+            {name : "unitInfo", label : "Unit Info", component : UnitInfo,},
+            {name : "pilots", label : "Pilots", component : Pilots,},
+            {name : "mechs", label : "Mechs", component : Mechs,},
+            {name : "unitOrganization", label : "Unit Organization", component : UnitOrganization}
         ];
 
         return (
