fb2f7cc8be3ba6a678832ac35ee7135585b66fc6
Mark Erikson <mark@isquaredsoftware.com>
2016-11-16 21:24:58 -0500

    Add initial form layout for UnitInfo

--- src/features/unitInfo/UnitInfo/UnitInfo.jsx
+++ src/features/unitInfo/UnitInfo/UnitInfo.jsx
@@ -1,5 +1,33 @@
 import React, {Component} from "react";
+import {Form, Dropdown, Segment} from "semantic-ui-react";
 
-const UnitInfo = () => <div>Unit Info content</div>;
+const FACTIONS = [
+    {value : "cc", text : "Capellan Confederation"},
+    {value : "dc", text : "Draconis Combine"},
+    {value : "fs", text : "Federated Suns"},
+    {value : "fwl", text : "Free Worlds League"},
+    {value : "lc", text : "Lyran Commonwealth"},
+];
+
+const UnitInfo = () => {
+
+    return (
+        <Segment attached="bottom">
+            <Form size="large">
+                <Form.Field name="name" width={6} >
+                    <label>Unit Name</label>
+                    <input placeholder="Name" />
+                </Form.Field>
+                <Form.Field name="affiliation" width={6}>
+                    <label>Affiliation</label>
+                    <Dropdown
+                       selection
+                       options={FACTIONS}
+                    />
+                </Form.Field>
+            </Form>
+        </Segment>
+    );
+}
 
 export default UnitInfo;
