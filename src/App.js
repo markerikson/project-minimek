059c2a73be827ec90e870e9e55b8f5518b69a990
Mark Erikson <mark@isquaredsoftware.com>
2016-11-20 15:28:21 -0500

    Use Semantic-UI and clean up unused code
    Set page title in HTML template
    Removed unused index.css
    Removed logo from header and removed unused logo file
    Shrank header and removed unused CSS
    Imported Semantic-UI CSS into index
    Added a Semantic-UI Header into the page header
    Removed SampleComponent from App and removed the file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+    Header,
+} from "semantic-ui-react";
 
-import SampleComponent from "./SampleComponent";
+
+import './App.css';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h2>Project Mini-Mek</h2>
+                    <Header inverted as="h1">Project Mini-Mek</Header>
                 </div>
-                <SampleComponent />
             </div>
         );
     }
