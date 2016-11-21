47154d0cd195f5e565af7056ea99751b113a6694
Mark Erikson <mark@isquaredsoftware.com>
2016-11-20 19:34:22 -0500

    Render the list of tabs using TabBarContainer

--- src/App.js
+++ src/App.js
@@ -2,26 +2,29 @@ import React, { Component } from 'react';
 import {
     Header,
     Container,
-    Menu,
 } from "semantic-ui-react";
+
+import TabBarContainer from "./features/tabs/TabBarContainer";
 
 
 import './App.css';
 
 class App extends Component {
     render() {
+        const tabs = [
+            {name : "unitInfo", label : "Unit Info"},
+            {name : "pilots", label : "Pilots"},
+            {name : "mechs", label : "Mechs"},
+            {name : "unitOrganization", label : "Unit Organization"}
+        ];
+
         return (
             <div className="App">
                 <div className="App-header">
                     <Header inverted as="h1">Project Mini-Mek</Header>
                 </div>
                 <Container>
-                    <Menu tabular size="massive">
-                        <Menu.Item name="unitInfo" active={true}>Unit Info</Menu.Item>
-                        <Menu.Item name="pilots" active={false}>Pilots</Menu.Item>
-                        <Menu.Item name="mechs" active={false}>Mechs</Menu.Item>
-                        <Menu.Item name="unitOrganization" active={false}>Unit Organization</Menu.Item>
-                    </Menu>
+                    <TabBarContainer tabs={tabs} size="massive" />
                 </Container>
             </div>
         );
