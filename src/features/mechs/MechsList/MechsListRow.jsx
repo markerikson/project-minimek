f2594dedfad8a7823803785294ef4b2df2140883
Mark Erikson <mark@isquaredsoftware.com>
2016-11-30 22:46:31 -0500

    Extract components from Mechs panel, and add initial test data
    Extracted MechsList plus child components
    Extracted MechDetails
    Added a test data entry to Mechs state, and passed to other components
    Added getWeightClass selector, and used for rendering

---
+++ src/features/mechs/MechsList/MechsListRow.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {Table} from "semantic-ui-react";
+
+import {getWeightClass} from "../mechSelectors";
+
+
+const MechsListRow = ({id, name, type, weight}) => {
+    const weightClass = getWeightClass(weight);
+
+    return (
+        <Table.Row>
+            <Table.Cell>
+                {id}
+            </Table.Cell>
+            <Table.Cell>
+                {name}
+            </Table.Cell>
+            <Table.Cell>
+                {type}
+            </Table.Cell>
+            <Table.Cell>
+                {weight}
+            </Table.Cell>
+            <Table.Cell>
+                {weightClass}
+            </Table.Cell>
+        </Table.Row>
+    );
+}
+
+
+export default MechsListRow;
