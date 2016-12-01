3ccddf180c939351fa8074144d9e2a4c97f60621
Mark Erikson <mark@isquaredsoftware.com>
2016-11-30 23:55:54 -0500

    Load unit details from sample data

--- src/features/unitInfo/unitInfoReducer.js
+++ src/features/unitInfo/unitInfoReducer.js
@@ -1,12 +1,18 @@
 import {createReducer} from "common/utils/reducerUtils";
 
+import {DATA_LOADED} from "features/tools/toolConstants";
 
 const initialState = {
-    name : "Black Widow Company",
-    affiliation : "wd",
+    name : "N/A",
+    affiliation : "",
 };
 
+function dataLoaded(state, payload) {
+    const {unit} = payload;
+
+    return unit;
+}
 
 export default createReducer(initialState, {
-    
+    [DATA_LOADED] : dataLoaded,
 });
