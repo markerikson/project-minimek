5e9e1c441d60ccf1023cd36de1fc490518c5f375
Mark Erikson <mark@isquaredsoftware.com>
2016-11-30 21:44:42 -0500

    Extract components from Pilots panel, and add initial test data
    Extracted PilotsList plus child components
    Extracted PilotDetails
    Added a test data entry to Pilots state, and passed to other components

---
+++ src/features/pilots/PilotsList/PilotsListRow.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {Table} from "semantic-ui-react";
+
+const PilotsListRow = ({name, rank, age, gunnery, piloting, mechType}) => (
+    <Table.Row>
+        <Table.Cell>
+            {name}
+        </Table.Cell>
+        <Table.Cell>
+            {rank}
+        </Table.Cell>
+        <Table.Cell>
+            {age}
+        </Table.Cell>
+        <Table.Cell>
+            {gunnery}/{piloting}
+        </Table.Cell>
+        <Table.Cell>
+            {mechType}
+        </Table.Cell>
+    </Table.Row>
+);
+
+export default PilotsListRow;
