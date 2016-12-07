1e52982454395ef1b666adf2a9add6ec6582dd69
Mark Erikson <mark@isquaredsoftware.com>
2016-12-06 21:57:15 -0500

    Add loading of pilots from sample data

--- src/app/reducers/entitiesReducer.js
+++ src/app/reducers/entitiesReducer.js
@@ -1,11 +1,23 @@
 import {createReducer} from "common/utils/reducerUtils";
 
+import {DATA_LOADED} from "features/tools/toolConstants";
 
 import schema from "app/schema"
 
 const initialState = schema.getDefaultState();
 
+export function loadData(state, payload) {
+    const session = schema.from(state);
+    const {Pilot} = session;
+
+    const {pilots} = payload;
+    pilots.forEach(pilot => Pilot.parse(pilot));
+
+    return session.reduce();
+}
+
+
 
 export default createReducer(initialState, {
-    
+    [DATA_LOADED] : loadData,
 });
--- src/features/pilots/Pilot.js
+++ src/features/pilots/Pilot.js
@@ -3,6 +3,15 @@ import {Model} from "redux-orm";
 
 export default class Pilot extends Model {
 
+    static parse(pilotData) {
+        // We could do useful stuff in here with relations,
+        // but since we have no relations yet, all we need
+        // to do is pass the pilot data on to create()
+
+        // Note that in a static class method, `this` is the
+        // class itself, not an instance
+        return this.create(pilotData);
+    }
 }
 
 Pilot.modelName = "Pilot";
