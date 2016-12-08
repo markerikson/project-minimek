3b25f24b5b7b74dd11d27d58134b11b35802a527
Mark Erikson <mark@isquaredsoftware.com>
2016-12-07 21:33:08 -0500

    Load Mechs and MechDesigns from sample data and use in display
    Added Mech and MechDesign entries to the sample data
    Added reducer logic to load them into the store
    Updated Pilots mapState to use relations to look up mech type

--- src/app/reducers/entitiesReducer.js
+++ src/app/reducers/entitiesReducer.js
@@ -9,12 +9,15 @@ const initialState = schema.getDefaultState();
 export function loadData(state, payload) {
     // Create a Redux-ORM session from our entities "tables"
     const session = schema.from(state);
-    // Get a reference to the correct version of the Pilots class for this Session
-    const {Pilot} = session;
+    // Get a reference to the correct version of model classes for this Session
+    const {Pilot, MechDesign, Mech} = session;
 
-    const {pilots} = payload;
-    // Queue up creation commands for each pilot entry
+    const {pilots, designs, mechs} = payload;
+
+    // Queue up creation commands for each entry
     pilots.forEach(pilot => Pilot.parse(pilot));
+    designs.forEach(design => MechDesign.parse(design));
+    mechs.forEach(mech => Mech.parse(mech));
 
     // Apply the queued updates and return the updated "tables"
     return session.reduce();
