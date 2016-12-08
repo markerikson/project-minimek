d1aa515d85f09764c72d7e81c6bab31c464fd77f
Mark Erikson <mark@isquaredsoftware.com>
2016-12-07 23:12:02 -0500

    Add logic for tracking the currently selected pilot

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -3,11 +3,13 @@ import {combineReducers} from "redux";
 import entitiesReducer from "./entitiesReducer";
 import tabReducer from "features/tabs/tabReducer";
 import unitInfoReducer from "features/unitInfo/unitInfoReducer";
+import pilotsReducer from "features/pilots/pilotsReducer";
 
 
 const rootReducer = combineReducers({
     entities : entitiesReducer,
     unitInfo : unitInfoReducer,
+    pilots : pilotsReducer,
     tabs : tabReducer,
 });
 
---
+++ src/features/pilots/pilotsConstants.js
@@ -0,0 +1,1 @@
+export const PILOT_SELECT = "PILOT_SELECT";
