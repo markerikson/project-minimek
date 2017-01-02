d1488c8130652e2734b8783b0570e6cfedbde664
Mark Erikson <mark@isquaredsoftware.com>
2017-01-02 17:52:07 -0500

    Add a utility function to extract values from input events

---
+++ src/common/utils/clientUtils.js
@@ -0,0 +1,19 @@
+import {isObject} from "lodash";
+
+export function getValueFromEvent(e) {
+    const {target} = e;
+
+    let newValues;
+
+    if(target) {
+        const value = (target.type === "checkbox") ? target.checked : target.value;
+        newValues = {
+            [target.name] : value,
+        };
+    }
+    else if(isObject(e)) {
+        newValues = e;
+    }
+
+    return newValues;
+}
