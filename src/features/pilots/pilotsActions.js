09fda20959bdcad88252320e2874664c43b3e7a9
Mark Erikson <mark@isquaredsoftware.com>
2017-01-07 16:36:57 -0500

    Add logic to track if a pilot is being edited

--- src/features/pilots/pilotsActions.js
+++ src/features/pilots/pilotsActions.js
@@ -1,8 +1,24 @@
-import {PILOT_SELECT} from "./pilotsConstants";
+import {
+    PILOT_SELECT,
+    PILOT_EDIT_START,
+    PILOT_EDIT_STOP,
+} from "./pilotsConstants";
 
 export function selectPilot(pilotID) {
     return {
         type : PILOT_SELECT,
         payload : {currentPilot : pilotID},
+    };
+}
+
+export function startEditingPilot() {
+    return {
+        type : PILOT_EDIT_START,
+    };
+}
+
+export function stopEditingPilot() {
+    return {
+        type : PILOT_EDIT_STOP,
     };
 }
