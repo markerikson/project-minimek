b7520812ba5bfca6cd64871f9024c473b7e0421d
Mark Erikson <mark@isquaredsoftware.com>
2017-01-07 17:08:43 -0500

    Add basic handling for updating individual entities

--- src/common/utils/reducerUtils.js
+++ src/common/utils/reducerUtils.js
@@ -13,3 +13,24 @@ export function reduceReducers(...reducers) {
             previous
         );
 }
+
+
+export function createConditionalSliceReducer(sliceName, fnMap) {
+    // Create a reducer that knows how to handle one slice of state, with these action types
+    const sliceReducer = createReducer({}, fnMap);
+
+    // Create a new wrapping reducer
+    return (state, action) => {
+        // Check to see if this slice reducer knows how to handle this action
+        if(fnMap[action.type]) {
+            // If it does, pass the slice to the slice reducer, and update the slice
+            return {
+                ...state,
+                [sliceName] : sliceReducer(state[sliceName], action),
+            };
+        }
+
+        // Otherwise, return the existing state unchanged
+        return state;
+    }
+}
