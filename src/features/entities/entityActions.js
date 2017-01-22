8c748dd7b29ef63721da02ccafccb9e2c083fb22
Mark Erikson <mark@isquaredsoftware.com>
2017-01-22 15:31:21 -0500

    Add generic entity creation and deletion handling

--- src/features/entities/entityActions.js
+++ src/features/entities/entityActions.js
@@ -1,4 +1,8 @@
-import {ENTITY_UPDATE} from "./entityConstants";
+import {
+    ENTITY_UPDATE,
+    ENTITY_DELETE,
+    ENTITY_CREATE,
+} from "./entityConstants";
 
 export function updateEntity(itemType, itemID, newItemAttributes) {
     return {
@@ -6,6 +10,23 @@ export function updateEntity(itemType, itemID, newItemAttributes) {
         payload : {
             itemType,
             itemID,
+            newItemAttributes,
+        },
+    };
+}
+
+export function deleteEntity(itemType, itemID) {
+    return {
+        type : ENTITY_DELETE,
+        payload : {itemType, itemID}
+    };
+}
+
+export function createEntity(itemType, newItemAttributes) {
+    return {
+        type : ENTITY_CREATE,
+        payload : {
+            itemType,
             newItemAttributes,
         },
     };
