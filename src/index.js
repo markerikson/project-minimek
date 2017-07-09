895c2e003aa2c0bd70401877f3323be49b8dfc39
Mark Erikson <mark@isquaredsoftware.com>
2017-07-09 15:15:31 -0400

    Remove custom error overlay in favor of CRA's built-in overlay

--- src/index.js
+++ src/index.js
@@ -24,29 +24,7 @@ let render = () => {
 };
 
 if(module.hot) {
-    // Support hot reloading of components
-    // and display an overlay for runtime errors
-    const renderApp = render;
-    const renderError = (error) => {
-        const RedBox = require("redbox-react").default;
-        ReactDOM.render(
-            <RedBox error={error} />,
-            rootEl,
-        );
-    };
-
-    // In development, we wrap the rendering function to catch errors,
-    // and if something breaks, log the error and render it to the screen
-    render = () => {
-        try {
-            renderApp();
-        }
-        catch(error) {
-            console.error(error);
-            renderError(error);
-        }
-    };
-
+    // Support hot reloading of components.
     // Whenever the App component file or one of its dependencies
     // is changed, re-import the updated component and re-render it
     module.hot.accept("./app/layout/App", () => {
