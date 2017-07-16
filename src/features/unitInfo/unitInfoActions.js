c0baa2f2843d0c023901fbd355a3a1aae8e93a3e
Mark Erikson <mark@isquaredsoftware.com>
2017-07-16 16:16:40 -0400

    Implement logic to set the unit color from a ColorPicker

--- src/features/unitInfo/unitInfoActions.js
+++ src/features/unitInfo/unitInfoActions.js
@@ -1,8 +1,18 @@
-import {UNIT_INFO_UPDATE} from "./unitInfoConstants";
+import {
+    UNIT_INFO_UPDATE,
+    UNIT_INFO_SET_COLOR,
+} from "./unitInfoConstants";
 
 export function updateUnitInfo(values) {
     return {
         type : UNIT_INFO_UPDATE,
         payload : values,
+    };
+}
+
+export function setUnitColor(color) {
+    return {
+        type : UNIT_INFO_SET_COLOR,
+        payload : {color}
     };
 }
