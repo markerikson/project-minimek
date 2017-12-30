d9a3ad0b17af0ca96e641a3b8fc56955c5c62431
Mark Erikson <mark@isquaredsoftware.com>
2017-12-30 17:53:59 -0500

    Add Lance model

--- src/app/schema/schema.js
+++ src/app/schema/schema.js
@@ -5,8 +5,9 @@ import MechDesign from "features/mechs/MechDesign";
 import Mech from "features/mechs/Mech";
 import Unit from "features/unitInfo/Unit";
 import Faction from "features/unitInfo/Faction";
+import Lance from "features/unitOrganization/Lance";
 
 const orm = new ORM();
-orm.register(Pilot, MechDesign, Mech, Unit, Faction);
+orm.register(Pilot, MechDesign, Mech, Unit, Faction, Lance);
 
 export default orm;
---
+++ src/features/unitOrganization/Lance.js
@@ -0,0 +1,17 @@
+import {Model, fk, many, attr} from "redux-orm";
+
+
+export default class Lance extends Model {
+    static modelName = "Lance";
+    
+    static fields = {
+        id : attr(),
+        name : attr(),
+        pilots : many("Pilot")
+    };
+
+    static parse(lanceData) {
+        return this.upsert(lanceData);
+    }
+
+}
