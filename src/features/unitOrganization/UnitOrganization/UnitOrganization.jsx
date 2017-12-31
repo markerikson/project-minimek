b8925a37c186e2b7f9b172e0e2a7fda887710e41
Mark Erikson <mark@isquaredsoftware.com>
2017-12-30 20:50:09 -0500

    Show faction affiliation and unit color in the Unit TOE tree

--- src/features/unitOrganization/UnitOrganization/UnitOrganization.jsx
+++ src/features/unitOrganization/UnitOrganization/UnitOrganization.jsx
@@ -14,44 +14,61 @@ import Lance from "./Lance";
 
 const mapState = (state) => {
     const session = getEntitiesSession(state);
-    const {Unit, Lance} = session;
+    const {Unit, Lance, Faction} = session;
 
-    let unit;
+    let unit, faction;
 
     const unitModel = Unit.all().first();
 
     if(unitModel) {
         unit = unitModel.ref;
+        faction = unitModel.affiliation.ref;
     }
 
     let lances = Lance.all().toRefArray().map(lance => lance.id);
 
-    return {unit, lances};
+    return {unit, faction, lances};
 }
 
 const UNKNOWN_UNIT = {name : "Unknown"};
 
-const UnitOrganization = ({unit = UNKNOWN_UNIT, lances = []}) => {
-    const {name} = unit;
+const UnitOrganization = ({unit = UNKNOWN_UNIT, faction = {}, lances = []}) => {
+    const {name, affiliation, color = "white"} = unit;
+    const {name : factionName} = faction;
 
     const lanceEntries = lances.map(lanceID => <Lance key={lanceID} lanceID={lanceID} />);
+
+    const colorBlock = <div
+        style={{
+            marginLeft : 10,
+            backgroundColor : color,
+            border : "1px solid black",
+            height : 20,
+            width : 40,
+        }}
+    />;
+
+    const displayText = factionName ? `${name} / ${factionName}` : name;
 
     return (
         <Segment>
             <Grid>
                 <Grid.Column width={10}>
                     <Header as="h3">Unit Table of Organization</Header>
-                    <List size="large">
-                        <List.Item>
-                            <List.Icon name="cubes" />
-                            <List.Content>
-                                <List.Header>{name}</List.Header>
-                                <List.List>
-                                    {lanceEntries}
-                                </List.List>
-                            </List.Content>
-                        </List.Item>
-                    </List>
+                    <Segment>
+                        <List size="large">
+                            <List.Item>
+                                <List.Icon name="cubes" />
+                                <List.Content>
+                                    <List.Header style={{display : "flex"}}>{displayText} {colorBlock}</List.Header>
+                                    <List.List>
+                                        {lanceEntries}
+                                    </List.List>
+                                </List.Content>
+                            </List.Item>
+                        </List>
+                    </Segment>
+
                 </Grid.Column>
             </Grid>
         </Segment>
