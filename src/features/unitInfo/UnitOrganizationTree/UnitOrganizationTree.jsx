d1d5a75a2381bd085f3ce3bb9309136e5160e044
Mark Erikson <mark@isquaredsoftware.com>
2018-01-01 16:15:33 -0500

    Show hardcoded lances by ID in the Unit TOE tree

--- src/features/unitInfo/UnitOrganizationTree/UnitOrganizationTree.jsx
+++ src/features/unitInfo/UnitOrganizationTree/UnitOrganizationTree.jsx
@@ -4,8 +4,7 @@ import {
     List,
 } from "semantic-ui-react";
 
-import LancePilot from "./LancePilot";
-
+import Lance from "./Lance";
 
 const UnitOrganizationTree = () => {
     return (
@@ -15,44 +14,9 @@ const UnitOrganizationTree = () => {
                 <List.Content>
                     <List.Header>Black Widow Company</List.Header>
                     <List.List>
-                        <List.Item>
-                            <List.Icon name="cube" />
-                            <List.Content>
-                                <List.Header>Command Lance</List.Header>
-                                <List.List>
-                                    <LancePilot pilotID={1} />
-                                    <LancePilot pilotID={2} />
-                                    <LancePilot pilotID={3} />
-                                    <LancePilot pilotID={4} />
-                                </List.List>
-                            </List.Content>
-                        </List.Item>
-
-                        <List.Item>
-                            <List.Icon name="cube" />
-                            <List.Content>
-                                <List.Header>Fire Lance</List.Header>
-                                <List.List>
-                                    <LancePilot pilotID={5} />
-                                    <LancePilot pilotID={6} />
-                                    <LancePilot pilotID={7} />
-                                    <LancePilot pilotID={8} />
-                                </List.List>
-                            </List.Content>
-                        </List.Item>
-
-                        <List.Item>
-                            <List.Icon name="cube" />
-                            <List.Content>
-                                <List.Header>Recon Lance</List.Header>
-                                <List.List>
-                                    <LancePilot pilotID={9} />
-                                    <LancePilot pilotID={10} />
-                                    <LancePilot pilotID={11} />
-                                    <LancePilot pilotID={12} />
-                                </List.List>
-                            </List.Content>
-                        </List.Item>
+                        <Lance lanceID={1} />
+                        <Lance lanceID={2} />
+                        <Lance lanceID={3} />
                     </List.List>
                 </List.Content>
             </List.Item>
